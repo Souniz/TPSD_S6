2d9eb8fbc8b37c41b3b051316d022ab79ff8dfdc
Souniz <sounaniang10@gmail.com>
2024-03-15 16:29:38 +0100

    Debut TP5 TBD

--- TP_Theo_Bayesien/Tp2_1.py
+++ TP_Theo_Bayesien/Tp2_1.py
@@ -7,7 +7,7 @@ from tools import  show,test
 #2)-------Utilisation des outils sur de nouveaux jeux de donnees--------------
 """
 
-show('tp2_data/tp2_data1_train.txt')
+#show('tp2_data/tp2_data1_train.txt')
 frontiere1=55
 print(test('tp2_data/tp2_data1_valid.txt',frontiere1))
 """
