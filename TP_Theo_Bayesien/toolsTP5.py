cf9255b9acd89746f63042520fbbc52ddb68634d
Souniz <sounaniang10@gmail.com>
2024-03-21 14:40:25 +0100

    Tp5 TBD v3

--- TP_Theo_Bayesien/toolsTP5.py
+++ TP_Theo_Bayesien/toolsTP5.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import utils as ut
-df=pd.read_csv('tp5_data/tp5_data1_valid.txt',names=['x1','x2','y'])
+df=pd.read_csv('tp5_data/tp5_data2_valid.txt',names=['x1','x2','y'])
 X_train=df[['x1','x2']]
 Y_train=np.array(df['y'])
 couleur={1:'blue',0:'orange'}
-def prediction(x):
-    #f(x)=x-20
-    if x[1]>x[0]-20:
+def prediction1(x):
+    #f(x)=x-400
+    if x[1]> 0.6*x[0]+290:
         return 0
     else:
         return 1
@@ -16,7 +16,11 @@ plt.figure(figsize=(12,8))
 for label in np.unique(Y_train):
     plt.scatter(df[Y_train == label]['x1'], df[Y_train == label]['x2'], label=label, marker='+' if label == 0 else 'x')
 plt.legend()
-ut.plot_decision(X_train['x1'].min(),X_train['x1'].max(),X_train['x2'].min(),X_train['x2'].max(),prediction=prediction)
+ut.plot_decision(X_train['x1'].min(),X_train['x1'].max(),X_train['x2'].min(),X_train['x2'].max(),prediction=prediction1)
 plt.axis('equal')
 plt.show()
+y_pred=[prediction1(i) for i in np.array(X_train)]
+mat_confusion2,Taux2=ut.create_mat(y_pred,Y_train)
+print(f"{Taux2} %")
+print(mat_confusion2)
 
